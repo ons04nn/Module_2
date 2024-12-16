30272ad64fcee074d8848c5522ef5d9548d6b663
ons04nn <30283118+ons04nn@users.noreply.github.com>
2024-12-16 18:37:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Спасибо за интерес! Этот раздел находится в разработке.');
+    });
+});
