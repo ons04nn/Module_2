808d4f1c54deb7d14f955d3e0c4401b35ac4897c
ons04nn <30283118+ons04nn@users.noreply.github.com>
2024-12-26 17:57:54 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.button').forEach(button => {
-  button.addEventListener('click', () => {
-    alert('Спасибо за интерес! Этот раздел находится в разработке.');
-  });
-});
